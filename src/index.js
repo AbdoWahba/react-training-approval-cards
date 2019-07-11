900f979181959b2183758b324df1f57ad9b1bdb8
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-11 18:27:58 +0200

    component reuse passing children peops

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import Faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        auther="Abdo"
-        date="today at 4:00 PM"
-        body="nice day"
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        auther="5alty"
-        date="today at 3:00 PM"
-        body="hi there"
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        auther="cotonily"
-        date="yesterday at 4:54 PM"
-        body="thank you"
-        avatar={Faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <div>are you sure ?</div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          auther="Abdo"
+          date="today at 4:00 PM"
+          body="nice day"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          auther="Ahmed"
+          date="today at 3:00 PM"
+          body="hi there"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          auther="Hussien"
+          date="yesterday at 4:54 PM"
+          body="thank you"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

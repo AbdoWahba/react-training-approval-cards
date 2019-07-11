7fe8a41f436347694adf53270d045ce516b8a3ef
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-11 15:19:34 +0200

    naive component approach making comment component and copy it to reuse it

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>hello there !</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src="" />
+        </a>
+        <div className="content">
+          <a href="/" className="auther">
+            Abdo
+          </a>
+          <div className="metadata">
+            <span className="date">today at 6:00PM</span>
+          </div>
+          <div className="text">hey there</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("div"));

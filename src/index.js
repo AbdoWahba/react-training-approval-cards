e0d1e0767299121e74ee6c66c64f59832448ea72
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-11 02:25:40 +0200

    adding CSS library -semantic UI- to the project downloading semantic.min.css from semantic UI cdn link the style file to index.html testing if file works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>hello there</div>;
+  return <div>hello there !</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector("div"));

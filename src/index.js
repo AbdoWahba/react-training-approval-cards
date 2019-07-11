a16da621156a15dd4611d9ec3d4baab7abc4f5a2
AbdoWahba <abdo.tw20154@gmail.com>
2019-07-11 16:08:46 +0200

    extracting js to new components and make component nesting

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Faker from "faker";
+import CommentDetails from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={Faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="auther">
-            Abdo
-          </a>
-          <div className="metadata">
-            <span className="date">today at 6:00PM</span>
-          </div>
-          <div className="text">hey there</div>
-        </div>
-      </div>
+      <CommentDetails />
     </div>
   );
 };
